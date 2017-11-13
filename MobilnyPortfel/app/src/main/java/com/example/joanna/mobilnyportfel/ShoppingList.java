83cef7ba2d28c1c26f5544da49f13bc544dda224
Łukasz Wasilewski <lukasz.wasilewski0395@gmail.com>
2017-11-13 22:31:05 +0100

    shoppinglist.java added

---
+++ MobilnyPortfel/app/src/main/java/com/example/joanna/mobilnyportfel/ShoppingList.java
@@ -0,0 +1,20 @@
+package com.example.joanna.mobilnyportfel;
+
+import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class ShoppingList extends AppCompatActivity {
+
+
+    @Override
+    protected void  onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shopping_list);
+    }
+
+
+}
